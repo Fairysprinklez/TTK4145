11c8f289067fef34c88281d16a02896f6cde8be0
oggbogg <Walidfaryabi@hotmail.com>
2017-02-15 16:12:32 +0100

    some more work on network, and excercise 6

--- project/networkTest.go
+++ project/networkTest.go
@@ -4,21 +4,17 @@ import (
 	"./network"
 	"flag"
 	"fmt"
-	"os"
 	"time"
 )
 
-type Message struct {
-	Msg string
-	Iter int
-}
+
 
 
 func main() {
 
 
-	messageTx := make(chan Message)
-	messageRx := make(chan Message)
+	messageTx := make(chan network.Message)
+	messageRx := make(chan network.Message)
 	// Our id can be anything. Here we pass it on the command line, using
 	//  `go run main.go -id=our_id`
 	var identity string
@@ -29,7 +25,7 @@ func main() {
 	go network.ConnectToNetwork(messageTx, messageRx)
 
 	go func() {
-		testMsg := Message{"Hello from " + identity, 0}
+		testMsg := network.Message{"Hello from " + identity, 0}
 		for {
 			testMsg.Iter++
 			go network.BroadcastMessage(testMsg, messageTx)
